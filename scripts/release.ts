d0f0c672f02cc1e02b18dd5ad197b3e461b0d8b6
alirezahematidev <alirezahematideveloper71@gmail.com>
2024-07-02 17:48:35 +0330

    init

---
+++ scripts/release.ts
@@ -0,0 +1,19 @@
+import { release } from "@vitejs/release-scripts";
+import type { Options as ExecaOptions } from "execa";
+import { execa } from "execa";
+
+async function run(bin: string, args: string[], opts: ExecaOptions = {}) {
+  return execa(bin, args, { stdio: "inherit", ...opts });
+}
+
+release({
+  repo: "furiosa-lib",
+  packages: ["form"],
+  toTag: (pkg, version) => `${pkg}@${version}`,
+  logChangelog: () => {},
+  generateChangelog: async (pkgName) => {
+    const args = ["--lerna-package", `@furiosa/${pkgName}`];
+
+    await run("npx", args, { cwd: `packages/${pkgName}` });
+  },
+});
